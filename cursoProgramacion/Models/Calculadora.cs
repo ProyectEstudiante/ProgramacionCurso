b05b71bc56bb3f0b4e2c5f17a01cae4874f93fe5
ProyectEstudiante <luisdaniel960824@gmail.com>
2023-10-09 02:36:40 -0400

    creacion de calculadora Model

---
+++ cursoProgramacion/Models/Calculadora.cs
@@ -0,0 +1,9 @@
+namespace cursoProgramacion.Models;
+
+public class CalculadoraViewModel
+{
+    public int Operador1 { get; set; }
+    public int Operador2 { get; set; }
+    public string Accion { get; set; }
+    public int Respuesta { get; set; }
+}
