8766d92b80cbea9fa529c40acb13dc009c3c7346
ProyectEstudiante <luisdaniel960824@gmail.com>
2023-10-10 00:04:52 -0400

    Creacion de vista de calculadora

--- cursoProgramacion/Models/Calculadora.cs
+++ cursoProgramacion/Models/Calculadora.cs
@@ -4,6 +4,6 @@ public class CalculadoraViewModel
 {
     public int Operador1 { get; set; }
     public int Operador2 { get; set; }
-    public string Accion { get; set; }
+    public string? Accion { get; set; }
     public int Respuesta { get; set; }
 }
