dcce64a7a351b7f0ead8236093c36bd2cb95234a
ProyectEstudiante <luisdaniel960824@gmail.com>
2023-10-10 23:25:57 -0400

    Creacion de metodos

---
+++ cursoProgramacion/Models/Contacto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cursoProgramacion.Models
+{
+    public class Contacto
+    {
+        public string? Name { get; set; }
+        public string? Lasname { get; set; }
+        public string? Email { get; set; }
+        public int Numbre { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public string? Response { get; set; }
+        
+    }
+}
