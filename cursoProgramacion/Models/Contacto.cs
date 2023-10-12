4a7ee3e2054b6d37dcc37df3b266a395888e4dac
ProyectEstudiante <luisdaniel960824@gmail.com>
2023-10-11 21:15:29 -0400

    a;adiendo display a campos formularios

--- cursoProgramacion/Models/Contacto.cs
+++ cursoProgramacion/Models/Contacto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,17 @@ namespace cursoProgramacion.Models
 {
     public class Contacto
     {
+        [Display(Name="Nombre")]
         public string? Name { get; set; }
-        public string? Lasname { get; set; }
+        [Display(Name="Apellido")]
+        public string? Lastname { get; set; }
+        [Display(Name="Correo")]
         public string? Email { get; set; }
+        [Display(Name="Telefono")]
         public int Numbre { get; set; }
+        [Display(Name="Titulo")]
         public string? Title { get; set; }
+        [Display(Name="Mensage")]
         public string? Message { get; set; }
         public string? Response { get; set; }
         
